6d87cb23c41d5d942644da5b1d2d6e04d246dc25
Абдимомын Марат Сансызбайулы <AbdimomynM@lcoast.itstep>
2023-09-29 21:46:06 +0600

    'Update29092023'

--- app/views.py
+++ app/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from .forms import *
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import *
+from django.shortcuts import get_object_or_404
 
 class PictureView(ListView):
     model = Picture
@@ -20,3 +21,10 @@ class CreatePicture(CreateView):
     form_class = PictureForm
     template_name = 'create.html'
     success_url = reverse_lazy('home')
+
+class DeletePicture(DeleteView):    
+    model = Picture
+    template_name = 'delete.html'
+    pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('home')
+    
